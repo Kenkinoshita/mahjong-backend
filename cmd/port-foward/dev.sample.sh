90fa1934638f1e56d0c1265b23aec3331b06802d
Ken Kinoshita <kenkinoshita@MacBook-Pro-M4Max.local>
2026-09-05 15:41:56 +0900

    fix: dev.shをgitから外した

---
+++ cmd/port-foward/dev.sample.sh
@@ -0,0 +1,6 @@
+# 以下pemファイルのパスを適切なものに変更してください
+
+# ssh -L 5432:mahjong-db.cdkierbbje7a.ap-northeast-1.rds.amazonaws.com:5432 ec2-user@54.168.12.182 \
+#  -i /Users/kenkinoshita/.ssh/mahjong-db-jump-server-key.pem \
+#  -o ServerAliveInterval=3600 \
+#  -o ServerAliveCountMax=1
